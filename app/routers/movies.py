348e27647484f825910896eca4758847fccea1db
Julien ROBIN <julien.jean.robin@gmail.com>
2023-03-10 01:16:08 +0100

    movies/id and list pagination

--- app/routers/movies.py
+++ app/routers/movies.py
@@ -14,16 +14,55 @@ async def movies(sort: str = "popularity.desc", page: int = 1, lang: str = "en-U
     url = url_generator.create_url("/discover/movie", lang) + "&sort_by=" + sort + "&page=" + str(page)
 
     r = requests.get(url)
-    data = r.json()["results"]
+    data = r.json()
+    movies_json = data["results"]
 
     movies_list = []
-    for movie in data:
+    for movie_item in movies_json:
         movies_list.append({
-            "id": movie['id'],
-            "title": movie['title'],
-            "backdrop": url_generator.create_image_url(movie['backdrop_path'], "w780"),
-            "rating": movie['vote_average'],
-            "lang": movie['original_language']
+            "id": movie_item['id'],
+            "title": movie_item['title'],
+            "backdrop": url_generator.create_image_url(movie_item['backdrop_path'], "w780"),
+            "rating": movie_item['vote_average'],
+            "lang": movie_item['original_language'],
         })
 
-    return movies_list
+    res = {
+        "page": page,
+        "pages_nb": data["total_pages"],
+        "movies": movies_list
+    }
+
+    return res
+
+
+@router.get("/{movie_id}")
+async def movie(movie_id: int, lang: str = "en-US"):
+    url = url_generator.create_url("/movie/" + str(movie_id), lang)
+    r = requests.get(url)
+    data = r.json()
+
+    genres = []
+    for genre in data["genres"]:
+        genres.append(genre["name"])
+
+    producers = []
+    for producer in data["production_companies"]:
+        producers.append(producer["name"])
+
+    res = {
+        "id": data["id"],
+        "title": data["title"],
+        "original_title": data["original_title"],
+        "poster": url_generator.create_image_url(data["poster_path"], "w500"),
+        "backdrop": url_generator.create_image_url(data['backdrop_path'], "w780"),
+        "synopsis": data["overview"],
+        "genres": genres,
+        "rating": data['vote_average'],
+        "rating_count": data['vote_count'],
+        "lang": data["original_language"],
+        "producers": producers,
+        "release": data["release_date"],
+    }
+
+    return res
